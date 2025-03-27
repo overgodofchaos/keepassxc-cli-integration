615cd4af50b1167c3d9f575ca360caffe2374bbc
overgodofchaos <vezonzzzzzz@gmail.com>
2025-03-27 12:10:27 +0300

    v15

--- keepassxc_cli_integration/backend/dep.py
+++
@@ -1,3 +0,0 @@
-from .cmdargs import CmdArgs
-
-args = CmdArgs.get_args()
--- keepassxc_cli_integration/kpx_cmd.py
+++ keepassxc_cli_integration/kpx_cmd.py
@@ -1,68 +1,97 @@
 from keepassxc_cli_integration import kpx
-from keepassxc_cli_integration.backend import dep, utils, autorization
+from keepassxc_cli_integration.backend import utils, autorization
+import typer
+from typing import Literal
+from enum import Enum
+
+app = typer.Typer(
+    name="KeepassXC-CLI-Integration",
+    help="Getting data from a running KeepassXC-GUI instance.",
+    add_completion=False,
+    no_args_is_help=True,
+)
+
+
+class Value(str, Enum):
+    password = "password"
+    login = "login"
+
+
+@app.command(
+    help="Get value from kpx. To search for values in ALL open databases, you need to associate with each database."
+)
+def get(
+        value: Value = typer.Argument(
+            ...,
+            help="Select value: login, password"),
+
+        url: str = typer.Argument(
+            ...,
+            help="URL for item in keepassxc. Can be specified without http(s)://"),
+
+        name: str = typer.Option(
+            None,
+            help="Name of item (requred if one url has several items)"),
+
+        bat: bool = typer.Option(
+            False,
+            help="Escape answer for .bat scripts")
+):
+    try:
+        result = kpx.get_value(url, value.name, name)
+    except Exception as e:
+        print(e)
+        return
+
+    if bat:
+        print(utils.escape_for_bat(result))
+        return
+
+    print(result)
+
+
+associate_app = typer.Typer(
+    help="Associate with current active BD. Association management. (Default: add)",
+    no_args_is_help=False
+)
+app.add_typer(associate_app, name="associate")
+
+
+@associate_app.command(
+    help="Add current active DB to associaties"
+)
+def add():
+    kpx.associate()
+
+
+@associate_app.callback(invoke_without_command=True)
+def associate_default():
+    add()
+
+
+@associate_app.command(
+    help="Delete DB from associaties. (Default: current)"
+)
+def delete(
+        select: str = typer.Argument(
+            "current",
+            help="Accosiate name or 'current' or 'all'")
+):
+    match select:
+        case "current":
+            kpx.delete_association(current=True)
+        case "all":
+            kpx.delete_association(all_=True)
+        case _:
+            kpx.delete_association(id_=select)
+
+
+@associate_app.command(
+    help="Show all associaties"
+)
+def show():
+    print(autorization.read_settings_text())
 
 
 def main():
-    match dep.args.mode:
-        case "get":
-            url = dep.args.url
-            value = dep.args.value
-            name = dep.args.name
-
-            value = kpx.get_value(url, value, name)
-
-            if dep.args.bat:
-                print(utils.escape_for_bat(value))
-                return
-
-            print(value)
-
-        # case "associate":
-        #     if sum([
-        #         dep.args.delete is not None,
-        #         dep.args.clear,
-        #         dep.args.show
-        #     ]) == 0:
-        #         kpx.associate()
-        #         print("Association completed")
-        #         return
-        #
-        #     if dep.args.show:
-        #         print(autorization.read_settings_text())
-        #         return
-        #
-        #     if dep.args.clear:
-        #         kpx.delete_association(all_=True)
-        #         return
-        #
-        #     if dep.args.delete is not None:
-        #         id_ = dep.args.delete
-        #         if id_ == "":
-        #             id_ = "current"
-        #         if id_ in ["current"]:
-        #             kpx.delete_association(current=True)
-        #         else:
-        #             kpx.delete_association(id_=id_)
-        #         print(f"Association deleted: {id_}")
-        #         return
-
-        case "associate":
-            match [dep.args.command, dep.args.select]:
-                case "add":
-                    kpx.associate()
-
-                case "delete", select:
-                    match select:
-                        case "current":
-                            kpx.delete_association(current=True)
-                        case "all":
-                            kpx.delete_association(all_=True)
-                        case _:
-                            kpx.delete_association(id_=select)
-
-                case "show", select:
-                    print(autorization.read_settings_text())
-
-
-if __name__ == '__main__':
-    main()
+    app()
