419c27572a9535b68956a58ea313e1eed01be3ca
overgodofchaos <vezonzzzzzz@proton.me>
2026-04-24 14:10:34 +0300

    chore: delete keepassxc_protocol module

--- src/keepassxc_cli_integration/backend/kpx_protocol/__init__.py
+++
@@ -1,3 +0,0 @@
-from .classes_responses import Login
-from .connection_config import Associate, Associates
-from .kpx_protocol import Connection
--- src/keepassxc_cli_integration/backend/kpx_protocol/errors.py
+++
@@ -1,2 +0,0 @@
-class ResponseUnsuccesfulException(Exception):
-    pass
